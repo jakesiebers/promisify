577d16e35595e373b853a0c9e7364d93fbc799c8
Jake Siebers <jakesiebers@gmail.com>
2017-05-17 16:41:10 -0700

    don't reject if callback is used

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 
 const promisify = f => function() {
-  const p = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
 
     const callback = arguments[f.length - 1];
 
     arguments[f.length - 1] = (err, data) => {
-      if (err) {
-        reject(err);
+      if (callback){
+        callback(err, data);
       } else {
-        resolve(data);
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
       }
-
-      if (callback) callback(err, data);
     };
 
     arguments.length = f.length;
@@ -19,10 +21,6 @@ const promisify = f => function() {
     f.apply(this, Array.from(arguments));
 
   });
-
-  p.catch(() => {});
-  
-  return p;
 }
 
 module.exports = promisify;
